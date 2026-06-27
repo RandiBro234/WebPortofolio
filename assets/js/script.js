dea5e934614c3c10ffdf323a2c4d06a0896838f4
RandiBro234 <randizelda30@gmail.com>
2026-06-27 15:10:35 +0700

    Menghilangkan LOADER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,4 @@
 /* ==========================================
-   LOADER
-========================================== */
-
-window.addEventListener("load", () => {
-
-    const loader = document.getElementById("loader");
-
-    setTimeout(() => {
-
-        loader.style.opacity = "0";
-        loader.style.visibility = "hidden";
-
-    }, 800);
-
-});
-
-
-/* ==========================================
    DARK MODE
 ========================================== */
 
